b5e1e33099b0183fa8a6cce1180d94009f664161
ziyi19851021 <schacon@gmail.com>
2015-02-12 14:18:17 +0000

    add bin.py

--- bin.py
+++ bin.py
@@ -1,7 +1,8 @@
 #/usr/bin/env python
 
-import base64
+import base64,wave
 import urllib2
+import urllib
 
 file = open('./bin.txt','r')
 file1 = open('./indian.wav','w')
@@ -15,9 +16,40 @@ for i in range(12,len(context)-3):
 	#print a
 file.close()
 file1.close()
+gh_url = 'http://www.pythonchallenge.com/pc/hex/sorry.html'
 
-response = urllib2.Request('http://www.pythonchallenge.com/pc/hex/sorry.html')
-html = urllib2.urlopen(response)
-html1 = html.read()
+audio1 = wave.open('./indian.wav','r')
+audio2 = wave.open('./bin.wav','w')
+print audio1.getparams()
+audio2.setparams(audio1.getparams())
+print audio1.tell()
 
-print html1
+#audio2.setnchannels(2)
+#audio2.setnframes(audio1.getnframes()/2)
+audio2.setframerate(audio1.getframerate()/2)
+#audio1.setpos(audio1.getnframes()/2)
+
+audio2.writeframes(audio1.readframes(audio1.getnframes())[0::2])
+print audio2.getparams()
+audio1.close()
+audio2.close()
+
+'''
+password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+username = 'butter'
+password = 'fly'
+top_level_url = 'www.pythonchallenge.com:80'
+password_mgr.add_password(None,gh_url,username,password)
+handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+opener = urllib2.build_opener(handler)
+#opener.open(gh_url)
+urllib2.install_opener(opener)
+req = urllib2.Request(gh_url)
+response = urllib2.urlopen(req)
+page = response.read()
+print str(page)
+
+response = urllib.urlopen('http://www.pythonchallenge.com/pc/hex/sorry.html')
+page = response.read()
+print str(page)
+'''
