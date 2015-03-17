8daa6aaf38b49877a30aca80ba8205d79228c61c
ziyi19851021 <schacon@gmail.com>
2015-03-17 15:16:01 +0000

    finish lake

--- lake.py
+++ lake.py
@@ -17,22 +17,35 @@ def getwav(url, filename):
 
 	f = open(filename, 'w')
 	f.write(response.read())
-	f.close()
+	f.close() 
 
-def wav_change_img(filename, imgpath):
-	audio = wave.open(filename, 'r')
-	#print audio.getparams()
-	#print ord(audio.readframes(audio.getnframes())[10790])
-	data = audio.readframes(audio.getnframes())
-	img = Image.new('RGB', (60, 60),(255, 255, 255))
-	for i in range(0, 10800, 3):
-		print i
-		r = ord(data[i])
-		g = ord(data[i + 1])
-		b = ord(data[i + 2])
-		x = i/3
-		img.putpixel((x%60, x/60), (r, g, b))
-	img.save(imgpath)
+def wav_change_img(filename, imgpath): 
+	audio = wave.open(filename, 'r') 
+	#print audio.getparams() 
+	#print ord(audio.readframes(audio.getnframes())[10790]) 
+	data = audio.readframes(audio.getnframes()) 
+	img = Image.new('RGB', (60, 60),(255, 255, 255)) 
+	for i in range(0, 10800, 3): 
+		print i 
+		r = ord(data[i]) 
+		g = ord(data[i + 1]) 
+		b = ord(data[i + 2]) 
+		x = i/3 
+		img.putpixel((x%60, x/60), (r, g, b)) 
+		img.save(imgpath) 
+
+def connect(imgpath, num): 
+	f = Image.open('./lakewav/lake.png')
+	img = f.convert('RGB') 
+	img_rgb = Image.open(imgpath).convert('RGB') 
+	w = ((num-1)%5) * 60 
+	h = ((num-1)/5) * 60 
+	#print w, h
+	for i in range(60):
+		for j in range(60):
+			img.putpixel((w + i, h + j), img_rgb.getpixel((i, j)))
+			#print img.getpixel((w+i, h+j))
+	img.save('./lakewav/lake.png')
 
 if __name__ == '__main__':
 	prefix_url = 'http://www.pythonchallenge.com/pc/hex/'
@@ -40,12 +53,14 @@ if __name__ == '__main__':
 	prefix_file = 'lake'
 	suffix_file = '.wav'
 	suffix_img = '.png'
+	img = Image.new('RGB', (300, 300), (0, 0, 0))
+	img.save('./lakewav/lake.png')
 	for i in range(1,26):
 		filename = prefix_file + str(i) + suffix_file
-		print filename
 		url = prefix_url + filename
 		path = prefix_path + filename
 		imgpath = prefix_path + prefix_file + str(i) + suffix_img
 		#getwav(url, path)
-		wav_change_img(path, imgpath)
-
+		#wav_change_img(path, imgpath)
+		print imgpath
+		connect(imgpath, i)
